98cf8c10468f5a94ad974fc6a369e3df64da1916
muhammadpauzi <muhammadpauzi200@gmail.com>
2021-08-24 12:55:50 +0700

    Refactoring xhr request to be callback function

--- app.js
+++ app.js
@@ -12,26 +12,41 @@ function updateInputFile() {
     fileUploadName.textContent = this.files ? this.files[0].name : 'No file choosen';
 };
 
+function request(method, url, cb) {
+    const xhr = new XMLHttpRequest();
+    try {
+        xhr.open(method, url);
+        cb(xhr, null);
+    } catch (err) {
+        cb(null, err);
+    }
+}
+
 function uploadFile(e) {
     e.preventDefault();
 
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "http://localhost:5000/api/images/upload");
-    xhr.onreadystatechange = function (e) {
-        if (e.target.readyState === 4) {
-            inputFile.value = ""; // reset
-            updateInputFile(); // change content to "No file choosen"
-            message.textContent = JSON.parse(e.target.response).message;
-            if (successStatusCode.indexOf(e.target.status) != -1) { // success
-                message.className = 'message message-green';
-            } else if (errorStatusCode.indexOf(e.target.status) != -1) { // error
-                message.className = 'message message-red';
+    request('POST', "http://localhost:5000/api/images/upload", (xhr, err) => {
+        if (err) {
+            return console.log(err);
+        }
+
+        xhr.onreadystatechange = function (e) {
+            if (e.target.readyState === 4) {
+                inputFile.value = ""; // reset
+                updateInputFile(); // change content to "No file choosen"
+                message.textContent = JSON.parse(e.target.response).message;
+                if (successStatusCode.indexOf(e.target.status) != -1) { // success
+                    message.className = 'message message-green';
+                } else if (errorStatusCode.indexOf(e.target.status) != -1) { // error
+                    message.className = 'message message-red';
+                }
             }
         }
-    }
-    const formData = new FormData();
-    formData.append("file", inputFile.files[0]);
-    xhr.send(formData);
+
+        const formData = new FormData();
+        formData.append("file", inputFile.files[0]);
+        xhr.send(formData);
+    });
 }
 
 updateInputFile();
